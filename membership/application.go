3957a63763344a34bb7def21ec0611ee773ae547
moonyoung <moonyoung.dev@kakao.com>
2022-06-12 12:03:34 +0900

    feat: create member set up
    feat: update member set up 1
    
    feat: update member set up 2
    
    feat: update member set up 3
    
    feat: delete member set up

--- membership/application.go
+++ membership/application.go
@@ -7,3 +7,14 @@ type Application struct {
 func NewApplication(repository Repository) *Application {
 	return &Application{repository: repository}
 }
+
+func (app *Application) Create(request CreateRequest) (CreateResponse, error) {
+	return CreateResponse{"1", "naver"}, nil
+}
+
+func (app *Application) Update(request UpdateRequest) (UpdateResponse, error) {
+	return UpdateResponse{}, nil
+}
+func (app *Application) Delete(id string) error {
+	return nil
+}
