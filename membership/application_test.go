77b854c0bd7c8d6e7e6d4add737e4d178d3bc177
moonyoung <moonyoung.dev@kakao.com>
2022-06-12 12:08:27 +0900

    feat: delete member set up

--- membership/application_test.go
+++ membership/application_test.go
@@ -54,6 +54,19 @@ func TestUpdate(t *testing.T) {
 	})
 
 	t.Run("주어진 멤버쉽 타입이 아닌 경우, 예외 처리한다.", func(t *testing.T) {
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("멤버십을 삭제한다.", func(t *testing.T) {
+
+	})
+
+	t.Run("id를 입력하지 않았을 때 예외 처리한다.", func(t *testing.T) {
+
+	})
+
+	t.Run("입력한 id가 존재하지 않을 때 예외 처리한다.", func(t *testing.T) {
 
 	})
 }
