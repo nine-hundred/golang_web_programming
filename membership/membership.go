5d079a3bb34fe5bcfe8a3e5cfcc187a19573e878
nine-hundred <ninehundred@outlook.kr>
2022-06-17 11:36:07 +0900

    feat: 멤버십 생성 기능 추가

--- membership/membership.go
+++ membership/membership.go
@@ -5,3 +5,30 @@ type Membership struct {
 	UserName       string
 	MembershipType string
 }
+
+func (m *Membership) SetID(id string) *Membership {
+	m.ID = id
+	return m
+}
+
+func (m *Membership) SetUserName(userName string) *Membership {
+	m.UserName = userName
+	return m
+}
+
+func (m *Membership) SetMembershipType(membershipType string) *Membership {
+	m.MembershipType = membershipType
+	return m
+}
+
+func (m *Membership) GetMembership() Membership {
+	return Membership{
+		ID:             m.ID,
+		UserName:       m.UserName,
+		MembershipType: m.MembershipType,
+	}
+}
+
+func NewMembershipBuilder() *Membership {
+	return &Membership{}
+}
